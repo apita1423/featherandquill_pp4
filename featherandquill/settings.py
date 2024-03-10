87965afee1f5b7c989f4b66372c99edd34e1fb88
Amanda <apita1423@outlook.com>
2024-03-10 18:17:37 +0000

    Add procfile to directory

--- featherandquill/settings.py
+++ featherandquill/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-qntsj1j!nideewkc&13ixevh96queuduyix7u7itzp=o0qvv7)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
