1e066cc0d5e586659901c77462cedc9bf85cd192
Amanda <apita1423@outlook.com>
2024-03-12 20:32:32 +0000

    Add templates and static files with html and css

--- birdblog/views.py
+++ birdblog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Bird Blog")
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter()
+    template_name = "blog/index.html"
+    paginate_by = 6
--- featherandquill/urls.py
+++ featherandquill/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from birdblog import views as index_views
 
 urlpatterns = [
+    path("", include("birdblog.urls"), name="birdblog-urls"),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('', index_views.index, name='index')
 ]
