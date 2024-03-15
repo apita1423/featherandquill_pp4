2548afeac44cf490b3235ab07d777c632b587765
Amanda <apita1423@outlook.com>
2024-03-15 22:00:17 +0000

    Add different bootstrap theme to base.html

--- featherandquill/urls.py
+++ featherandquill/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("birdblog.urls"), name="birdblog-urls"),
     path('admin/', admin.site.urls),
+    path("", include("birdblog.urls"), name="birdblog-urls"),
     path('summernote/', include('django_summernote.urls')),
 ]
