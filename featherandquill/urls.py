2cf84ce4a10b8a700d36ba7d62fbf4a23603e216
Amanda <apita1423@outlook.com>
2024-03-25 14:57:24 +0000

    Finish the gallery section

--- featherandquill/urls.py
+++ featherandquill/urls.py
@@ -28,4 +28,6 @@ urlpatterns = [
     path("", include("birdblog.urls"), name="birdblog-urls"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 handler404 = 'birdblog.views.custom_404'
