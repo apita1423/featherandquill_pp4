4540a9cfb784d4a84e3dcf27647faa70cb810cb2
Amanda <apita1423@outlook.com>
2024-03-25 22:00:39 +0000

    Add == to blog post due to syntax error

--- birdblog/models.py
+++ birdblog/models.py
@@ -12,8 +12,7 @@ class Post(models.Model):
     """
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey
-    (User, on_delete=models.CASCADE, related_name="blog_posts")
+    author = models.ForeignKey(User, on_delete==models.CASCADE, related_name=="blog_posts")
     featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
